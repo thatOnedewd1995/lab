bd25d27f737e1f79f12b910f89e3f107b7d1df4e
thatOnedewd1995 <118570120+thatOnedewd1995@users.noreply.github.com>
2022-11-18 22:02:49 -0600

    Added Account.py

---
+++ account.py
@@ -0,0 +1,34 @@
+class Account:
+    account_name = " "
+    account_balance = 0
+    def __init__(self,name, balance):
+        self.__account_balance = balance
+        self.__account_name = name
+
+
+
+    def deposit(self,amount, balance):
+        self.__amount = amount
+        self.__account_balance = balance
+        if amount <= 0:
+            return False
+        else:
+            self.__account_balance = balance + amount
+            return True
+
+
+    def withdraw(self,amount):
+        if amount <= 0:
+            return False
+        else:
+            account_balance = account_balance - amount
+            return True
+
+
+    def get_balance(self):
+        return account_balance
+
+
+    def get_name(self):
+        return account_name
+
