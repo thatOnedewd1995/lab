00b87ed9987d19c75171efacd11a19566f822946
thatOnedewd1995 <118570120+thatOnedewd1995@users.noreply.github.com>
2022-11-20 12:11:23 -0600

    update account.py

--- account.py
+++ account.py
@@ -1,34 +1,26 @@
 class Account:
-    account_name = " "
-    account_balance = 0
-    def __init__(self,name, balance):
+    def __init__(self, name, balance=0) -> None:
         self.__account_balance = balance
         self.__account_name = name
 
-
-
-    def deposit(self,amount, balance):
-        self.__amount = amount
-        self.__account_balance = balance
+    def deposit(self, amount) -> bool:
         if amount <= 0:
             return False
         else:
-            self.__account_balance = balance + amount
+            self.__account_balance = self.__account_balance + amount
             return True
 
-
-    def withdraw(self,amount):
+    def withdraw(self, amount) -> bool:
         if amount <= 0:
             return False
+        elif amount > self.__account_balance:
+            return False
         else:
-            account_balance = account_balance - amount
+            self.__account_balance = self.__account_balance - amount
             return True
 
+    def get_balance(self) -> float:
+        return self.__account_balance
 
-    def get_balance(self):
-        return account_balance
-
-
-    def get_name(self):
-        return account_name
-
+    def get_name(self) -> str:
+        return self.__account_name
